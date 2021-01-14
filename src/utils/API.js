819fde03d5adccd724b3a32059c67601de9a6c58
Faisal Naveed <faisal@rnssol.com>
2021-01-14 11:20:39 +0500

    config file added

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,13 @@
 import axios from "axios";
 
+var API = process.env.REACT_APP_API_URL
+  ? process.env.REACT_APP_API_URL
+  : "http://192.168.100.14:8000/";
+
+
+console.log("API", API)
+
 export default axios.create({
-  baseURL: "http://192.168.100.14:8000/",
+  baseURL: API,
   responseType: "json",
 });
